83e02bdde53965d2d208f4a433a26e7650a386a2
Halle Nunes <137959338+nuneshalle@users.noreply.github.com>
2023-07-02 00:40:10 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 courses: list[str] = open("courses.txt", "r").read().splitlines()
 
 year_search = input("enter the program level (1, 2, 3, 4): ")
-suj_search = str.upper(input("enter the subject area (first 3 letters of the course code): "))
+code_search = str.upper(input("enter the subject code (first 3 letters of the course code): "))
 
 os.system('clear')
-print(f"year {str(year_search)} {suj_search} courses:\n")
+print(f"year {str(year_search)} {code_search} courses:\n")
 
 try:
-  results: list[None] = [print(course) for course in courses if int(course[3]) == int(year_search) and suj_search in course]
+  results: list[None] = [print(course) for course in courses if int(course[3]) == int(year_search) and code_search in course]
   print(f"\n{len(results)} results from {len(courses)} courses")
 except ValueError:
   print("please enter a year.")
